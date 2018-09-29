d8c3f1cfa63efb6e107df705af8b48920bf8fc76
KlimovaD <klimova.d27@yandex.ru>
2018-09-29 16:09:18 +0300

    task 1

---
+++ Task1_1.hs
@@ -0,0 +1,33 @@
+data Term = IntConstant{ intValue :: Int }    
+            | Variable{ varName :: String }    
+            | Add{ lAdd :: Term, rAdd :: Term } 
+            | Sub{ lSub :: Term, rSub :: Term } 
+            | Mul{ lMul :: Term, rMul :: Term } 
+            | Neg{ neg :: Term } 
+            deriving(Show,Eq)
+ 
+(<+>),(<->),(<*>) :: Term -> Term -> Term
+ 
+l <+> r = Add l r
+l <-> r = Sub l r
+l <*> r = Mul l r
+ 
+infixl 6 <+>,<->
+infixl 7 <*>
+ 
+replaceVar :: String -> Int -> Term -> Term
+replaceVar _ _ t@IntConstant{} = t 
+replaceVar s v Variable{ varName = s' } | s==s' = IntConstant v
+                                        | otherwise = Variable s'
+replaceVar s v Add{lAdd=l,rAdd=r} = Add (replaceVar s v l) (replaceVar s v r)
+replaceVar s v Sub{lSub=l,rSub=r} = Sub (replaceVar s v l) (replaceVar s v r)
+replaceVar s v Mul{lMul=l,rMul=r} = Mul (replaceVar s v l) (replaceVar s v r)
+replaceVar s v Neg{ neg=n} = Neg (replaceVar s v n)
+
+evaluate :: Term -> Either String Int
+evaluate IntConstant{..} = Right intValue
+evaluate Variable{..} = Left $ "There was a variable " ++ varName         
+evaluate Add{..} = (+) <$> (evaluate lAdd) A.<*> (evaluate rAdd)         
+evaluate Sub{..} = (-) <$> (evaluate lSub) A.<*> (evaluate rSub)         
+evaluate Mul{..} = (*) <$> (evaluate lMul) A.<*> (evaluate rMul)         
+evaluate Neg{..} = (negate) <$> (evaluate neg)
---
+++ Task1_2.hs
@@ -0,0 +1,38 @@
+import Data.Maybe
+
+-- является ли дата корректной с учётом количества дней в месяце и вискокосных годов?
+isDateCorrect :: Int -> Int -> Int -> Bool
+isDateCorrect d m y = d>0 && m>0 && m<13 && y>=0 && d <=
+    (if m `elem` [4,6,9,11] then 30
+     else 
+        if m == 2 then
+            if (y `mod` 400 == 0) || (y `mod` 100 /= 0) && 
+                (y `mod` 4 == 0) then 29
+            else 28
+        else 31)
+ 
+-- наибольший общий делитель двух чисел
+gcd :: Integer -> Integer -> Integer
+gcd a b | a == b = a
+        | a > b = if a `mod` b == 0 then b else gcd b (a - b)
+        | otherwise = gcd b a
+
+--Возведение числа в степень
+pow :: Integer -> Integer -> Integer
+pow x y | y == 0 = 1
+	 | x == 0 = 0
+	 | y `mod` 2 == 1 = x * pow x (y - 1)
+	 | y `mod` 2 == 0 = pow (x * x) (y `div` 2) 
+
+-- является ли данное число простым
+isPrime :: Integer -> Bool
+isPrime x = factors x == [1,x] where factors x = [n | n <- [1..x], mod x n == 0]
+
+-- рассчитайте площадь многоугольника
+data Point2D = Point2D {x :: Double, y :: Double}
+shapeArea :: [Point2D] -> Double
+shapeArea [] = 0
+shapeArea lst = 
+    let xs = map x lst
+        ~ys@(yh:yt) = map y lst
+    in 0.5 * abs (sum [xi*(yp-yn)|(xi,yp,yn) <- zip3 xs (yt ++ [yh]) (last ys : ys)])
