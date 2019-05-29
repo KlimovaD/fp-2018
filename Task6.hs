0010c7ae3775d4fe659da7c15a465cbf22677dee
KlimovaD <klimova.d27@yandex.ru>
2019-05-29 18:49:13 +0300

    task 6 edited

--- Task6.hs
+++ Task6.hs
@@ -15,24 +15,30 @@ find (Node v l r p) a | v > a = find l a
                       | v < a = find r a
                       | v == a = True
 
-newParent Empty a p0 = Node a Empty Empty p0 
-newParent (Node v l r p) a p0 | a < v = let n = Node v (newParent l a n) r p0 in n
-                              | otherwise = let n = Node v l (newParent r a n) p0 in n
+newParent Empty _ = Empty 
+newParent (Node v l r p) n = Node v l r n
 
 insert ::(Ord a) => LinkedTree a -> a -> LinkedTree a
 insert Empty a = Node a Empty Empty Empty
-insert (Node v l r p) a | a < v = let n = Node v (newParent l a n) r p in n
-                        | otherwise = let n = Node v l (newParent r a n) p in n
+insert (Node v l r p) a | a < v = n 
+                        | otherwise = n1 
+                        where n = Node v (insert l a) rt (insert p a) 
+                              n1 = Node v lt (insert r a) (insert p a) 
+                              lt = newParent l n1
+                              rt = newParent r n
+
 
 joinNode _ Empty Empty = Empty
-joinNode p Empty (Node v l r p0) = Node v l r p
-joinNode p (Node v l r p0) t = n where n = Node v l (joinNode n r t) p
+joinNode Empty (Node v l r p0) p = Node v l r p
+joinNode (Node v l r p0) t p = n where n = Node v l (joinNode r t n) p
 
 remove :: (Ord a) => LinkedTree a -> a -> LinkedTree a
 remove Empty _ = Empty
 remove (Node v l r p) a
-    | a < v = Node v (remove l a) r p
-    | a > v = Node v l (remove r a) p
-    | otherwise = joinNode p l r 
-
-
+    | a < v = n
+    | a > v = n1
+    | otherwise = joinNode l r p
+    where n = Node v (remove l a) rt (remove p a)
+          n1 = Node v lt (remove r a) (remove p a)
+          rt = newParent r n
+          lt = newParent l n1
